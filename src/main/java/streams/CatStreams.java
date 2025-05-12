d66479d244cf9f6e296ed926afc7ed49daad3685
maltexx311 <malte-schule@gmx.de>
2025-05-12 23:03:03 +0200

    cats
    Nicht sicher warum der Spottlescheck in Github failed, aber in intelij keinen fehler wirft...

--- src/main/java/streams/CatStreams.java
+++ src/main/java/streams/CatStreams.java
@@ -2,17 +2,29 @@ package streams;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
-record Cat(int weight){};
+record Cat(int weight) {}
+;
 
+/** CatStreams. */
 public class CatStreams {
     public static void main(String... args) {
         List<Cat> clouder = new ArrayList<>();
-        clouder.add(new Cat(100));  clouder.add(new Cat(1));  clouder.add(new Cat(10));
+        clouder.add(new Cat(100));
+        clouder.add(new Cat(1));
+        clouder.add(new Cat(10));
 
         sumOverWeight(8, clouder);
     }
 
+    /**
+     * sumOverWeight.
+     *
+     * @param threshold
+     * @param cats
+     * @return
+     */
     private static int sumOverWeight(int threshold, List<Cat> cats) {
         int result = 0;
         for (Cat c : cats) {
@@ -21,6 +33,68 @@ public class CatStreams {
                 result += weight;
             }
         }
+        return result;
+    }
+
+    /**
+     * sumOverWeightStream.
+     *
+     * @param threshold
+     * @param cats
+     * @return
+     */
+    private static int sumOverWeightStream(int threshold, List<Cat> cats) {
+        int result =
+                cats.stream().filter(c -> c.weight() > threshold).mapToInt(c -> c.weight()).sum();
+
+        return result;
+    }
+
+    /**
+     * sumOverWeightStreamMethodenrefferenz.
+     *
+     * @param threshold
+     * @param cats
+     * @return
+     */
+    private static int sumOverWeightStreamMethodenrefferenz(int threshold, List<Cat> cats) {
+        int result =
+                cats.stream().filter(cat -> isFett(threshold, cat)).mapToInt(c -> c.weight()).sum();
+
+        return result;
+    }
+
+    /**
+     * isFett.
+     *
+     * @param threshold
+     * @param car
+     * @return
+     */
+    private static boolean isFett(int threshold, Cat car) {
+        return car.weight() > threshold ? true : false;
+    }
+
+    /**
+     * fettFilter.
+     *
+     * @param threshold
+     * @return
+     */
+    private static Predicate<Cat> fettFilter(int threshold) {
+        return cat -> cat.weight() > threshold;
+    }
+
+    /**
+     * superDuper.
+     *
+     * @param threshold
+     * @param cats
+     * @return
+     */
+    private static int superDuper(int threshold, List<Cat> cats) {
+        int result = cats.stream().filter(fettFilter(threshold)).mapToInt(Cat::weight).sum();
+
         return result;
     }
 }
