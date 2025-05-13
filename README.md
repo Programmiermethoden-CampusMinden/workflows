6ae9e408eafe3c297d29be65db12add4b25d68ad
maltexx311 <malte-schule@gmx.de>
2025-05-13 21:51:52 +0200

    updated getParameterNames Methode

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # Hello World
+lesseW etlaM morF olleH <-
 
 Aufgabe pro Team/Breakout:
 
-1.  Fügt ein "Hello from Team X" per Pull-Request in das README.md ein
-2.  Baue die Methode `getParameterNamesJson` in `streams.StringAppend` um, so dass statt der `for`-Schleife und der `if`-Abfrage die Stream-API genutzt wird. Versuche durch geschicktes Auslagern von Code-Teilen in neue Methoden die Lambda-Ausdrücke komplett durch Methodenreferenzen zu ersetzen.
+1.  Fügt ein "Hello from Team Malte" per Pull-Request in das README.md ein
+2.  Baue die Methode `getParameterNamesJson` in `streams.StringAppend` um, so dass statt der `for`-Schleife und der
+3. `if`-Abfrage die Stream-API genutzt wird. Versuche durch geschicktes Auslagern von Code-Teilen in neue Methoden
+4.  die Lambda-Ausdrücke komplett durch Methodenreferenzen zu ersetzen.
 3.  Erstelle einen Pull-Request gegen das Ursprungs-Repo. Läuft die CI durch? Wenn nicht, was musst Du tun, damit der PR akzeptabel wird?
--- src/main/java/streams/StringAppend.java
+++ src/main/java/streams/StringAppend.java
@@ -1,6 +1,31 @@
 package streams;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class StringAppend {
+
+    public static void main(String[] args) {
+        //testen
+        String[] testString = {"eins", "zwei", "drei"};
+        System.out.println(getParameterNamesJson(testString));
+        System.out.println("---hubscher---");
+        System.out.println(getParameterNamesJsonStream(testString));
+        //ausgabe war:
+
+        //["", "", ""]
+        //---hubscher---
+        //["eins", "zwei", "drei"]
+
+        //welches ist dann aber falsch?
+    }
+
+    /**
+     * alte uncoole methode.
+     *
+     * @param parameterNames
+     * @return
+     */
     public static String getParameterNamesJson(String[] parameterNames) {
         if (parameterNames.length == 0) {
             return "[]";
@@ -21,5 +46,21 @@ public class StringAppend {
     private static String escapeJson(String parameterName) {
         // does something or another ...
         return "";
+    }
+
+    /**
+     * Updated Methode mit Stream.
+     *
+     * @param parameterNames
+     * @return
+     */
+    public static String getParameterNamesJsonStream(String[] parameterNames) {
+        if (parameterNames.length == 0) {
+            return "[]";
+        }
+
+        return Stream.of(parameterNames)
+                .map(s -> "\"" + s + "\"")
+                .collect(Collectors.joining(", ", "[", "]"));
     }
 }
